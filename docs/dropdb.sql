e90d9c9faee658f134bc3e980a141f70fac22930
hyunmin0809 <khm010809@naver.com>
2022-05-29 17:11:38 +0900

    create/drop tables

---
+++ docs/dropdb.sql
@@ -0,0 +1,8 @@
+use DB2022Team06;
+
+drop table db2022_playlist_situation;
+drop table db2022_playlist_season;
+drop table db2022_playlist_age;
+drop table db2022_playlist_user;
+drop table db2022_user;
+drop table db2022_music;
