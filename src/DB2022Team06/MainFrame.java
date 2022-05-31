2f1488fe403fbd091cc5484a73ed0643396bf7e0
hanby-choi <hanby.choi@ewhain.net>
2022-06-01 02:53:37 +0900

    fix: connect MainFrame and Recommend

--- src/DB2022Team06/MainFrame.java
+++ src/DB2022Team06/MainFrame.java
@@ -6,20 +6,25 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-class MyListener extends JFrame implements ActionListener{
+class MyListener implements ActionListener{
 	 
 	int btnNum;  // 어떤 버튼 눌렀는지 
 	Container contentpane; // 현재 활성화된 메인 컨텐트 팬
+	MainFrame mainframe;
+	JPanel panel; // mainframe의 모든 컴포넌트가 부착된 panel
 	
-	public MyListener(int btnNum, Container contentpane){
+	public MyListener(int btnNum, Container contentpane, MainFrame mainframe, JPanel panel){
 		this.btnNum = btnNum;
 		this.contentpane = contentpane;
+		this.mainframe = mainframe;
+		this.panel = panel;
 	}
     @Override
     public void actionPerformed(ActionEvent e) {
         JButton button = (JButton)e.getSource();
-        contentpane.setVisible(false); // 메인화면 내용 보이지 않게 하기
+        panel.setVisible(false); // 메인화면 내용 보이지 않게 하기
         
         switch(btnNum) {
         case 0:
@@ -43,7 +48,7 @@ class MyListener extends JFrame implements ActionListener{
         	break; 
         	
         case 5:
-        	new Recommend();
+        	new Recommend(contentpane, mainframe, panel);
         	break;
         	
         case 6:
@@ -58,18 +63,18 @@ class MyListener extends JFrame implements ActionListener{
 }
 
 public class MainFrame extends JFrame{
-	
 	String[] btnList = new String[]{"전체음악 목록","TOP 10","<HTML>나만의<br>플레이 리스트</HTML>","<HTML>추천<br>플레이 리스트</HTML>","음악 검색","음악 추천","음원 관리","마이페이지"};
 	public MainFrame() { //생성자
-		
 		setTitle("음악정보관리시스템 이플리 DBMS");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container contentpane = getContentPane();
+		JPanel panel = new JPanel();
+		contentpane.add(panel);
 		
 		int BTNSIZE = 250;
 		int OFFSET = 70;
 		Color EWHA_GREEN = new Color(0,70,42);
-		contentpane.setLayout(null);// 배치관리자 해제
+		panel.setLayout(null); // 배치관리자 해제
 		
 		// NullContainer는 컴포넌트의 절대적 크기,위치를 지정해주어야함.
 		// 지정해주지 않을시 화면에 출력 X
@@ -77,7 +82,7 @@ public class MainFrame extends JFrame{
 		var la = new JLabel("음악정보관리시스템 이플리 DBMS");
 		la.setLocation(125,0); 
 		la.setSize(250,100); 
-		contentpane.add(la);
+		panel.add(la);
 		
 		// 상단 버튼 두개 (전체음악목록, TOP10)
 		for(int i=0;i<=1;i++) {
@@ -86,8 +91,8 @@ public class MainFrame extends JFrame{
 			b.setSize(BTNSIZE,50);
 			b.setForeground(Color.WHITE); //글씨 색상
 			b.setBackground(EWHA_GREEN);
-			b.addActionListener(new MyListener(i, contentpane));
-			contentpane.add(b);
+			b.addActionListener(new MyListener(i, contentpane, this, panel));
+			panel.add(b);
 		}
 		
 		//중간 플레이 리스트 버튼 두개
@@ -97,8 +102,8 @@ public class MainFrame extends JFrame{
 			b.setSize(120,100);
 			b.setForeground(Color.WHITE); //글씨 색상
 			b.setBackground(EWHA_GREEN); 
-			b.addActionListener(new MyListener(i, contentpane));
-			contentpane.add(b);
+			b.addActionListener(new MyListener(i, contentpane, this, panel));
+			panel.add(b);
 		}
 		
 		//하단 나머지 버튼 세개 (검색/추천/관리)
@@ -108,8 +113,8 @@ public class MainFrame extends JFrame{
 			b.setSize(BTNSIZE,50);
 			b.setForeground(Color.WHITE); //글씨 색상
 			b.setBackground(EWHA_GREEN);
-			b.addActionListener(new MyListener(i, contentpane));
-			contentpane.add(b);
+			b.addActionListener(new MyListener(i, contentpane, this, panel));
+			panel.add(b);
 		}
 	
 		//마이페이지 버튼
@@ -118,8 +123,8 @@ public class MainFrame extends JFrame{
 		myPageBtn.setSize(100,50);
 		myPageBtn.setForeground(Color.WHITE); //글씨 색상
 		myPageBtn.setBackground(EWHA_GREEN);
-		myPageBtn.addActionListener(new MyListener(7, contentpane));
-		contentpane.add(myPageBtn);
+		myPageBtn.addActionListener(new MyListener(7, contentpane, this, panel));
+		panel.add(myPageBtn);
 		
 		setSize(450,800); //contentpane의 크기
 		setVisible(true); // 보이도록 하기
