cc052cd5aac8cc2ef47b5b7c683a382961899a35
hyunmin0809 <khm010809@naver.com>
2022-06-04 04:07:37 +0900

    fix: delete dropdb.sql & add drop view

--- docs/drop.sql
+++ docs/drop.sql
@@ -6,3 +6,6 @@ drop table db2022_songwriter;
 drop table db2022_playlist_user;
 drop table db2022_user;
 drop table db2022_music; 
+
+drop view db2022_all_song_adult;
+drop view db2022_all_song_minor;
--- docs/dropdb.sql
+++
@@ -1,8 +0,0 @@
-use DB2022Team06;
-
-drop table db2022_playlist_situation;
-drop table db2022_playlist_season;
-drop table db2022_playlist_age;
-drop table db2022_playlist_user;
-drop table db2022_user;
-drop table db2022_music;
